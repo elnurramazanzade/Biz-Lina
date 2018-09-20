8ca07821f75a21a7e8e88ba4dcb29a633b4b2569
elnurramazanzade <elnur.ramazanzade@mail.ru>
2018-09-21 00:38:08 +0400

    second commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-$(document).ready(function () {
-
-  $(".search-icon").click(function (e) {
+$(document).ready(function() {
+  // SCROLL FOR BODY
+  // $("#btn-scrollup").click(function(e) {
+  //   var linkHref = $(this).attr("href");
+  //   $("body,html").animate(
+  //     {
+  //       scrollTop: $(linkHref).offset().top
+  //     },
+  //     1000
+  //   );
+  // });
+  // SCROLL FOR NAVBAR
+  $(window).scroll(function() {
+    if ($(this).scrollTop() < 100) {
+      $("#btn-scrollup").css("display", "none");
+    } else {
+      $("#btn-scrollup").css("display", "block");
+    }
+  });
+  $(".search-icon").click(function(e) {
     e.preventDefault();
     $(".search-form").slideToggle();
   });
-  
 });
