dd3ba23b1b1f8e377b6cea3e795dd040b2c0ca7f
elnurramazanzade <elnur.ramazanzade@mail.ru>
2018-09-23 02:07:29 +0400

    contact

--- js/script.js
+++ js/script.js
@@ -52,9 +52,48 @@ $(document).ready(function() {
       0: {
         items: 1
       },
-      1000: {
+      600: {
         items: 2
       }
+    }
+  });
+  // FORM
+  $("#send").on("click", function(e) {
+    e.preventDefault();
+    var mesDiv = $("#message");
+    mesDiv.children().remove();
+    var message = $(`<div></div>`);
+
+    if ($("#name").val() == "") {
+      var span = $(`<span></span>`).text(`Please enter your name.`);
+      message.append(span);
+      mesDiv.append(message);
+      $(mesDiv).slideToggle("slow");
+    } else if ($("#email").val() == "") {
+      var span = $(`<span></span>`).text(`Please enter valid email address.`);
+      message.append(span);
+      mesDiv.append(message);
+      $(mesDiv).slideToggle("slow");
+    } else if ($("#text").val() == "") {
+      var span = $(`<span></span>`).text(`Please enter your message.`);
+      message.append(span);
+      mesDiv.append(message);
+      $(mesDiv).slideToggle("slow");
+    } else {
+      var name = $("#name").val();
+      var h2 = $(`<h2></h2>`).text(`Email Sent Successfully.`);
+      var p = $(`<p></p>`).text(
+        `Thank you ${name}, your message has been submitted to us.`
+      );
+      message.addClass("success");
+      message.append(h2);
+      message.append(p);
+      console.log(message);
+      mesDiv.append(message);
+      $(mesDiv).slideToggle("slow");
+      $("#contact")
+        .find("form")
+        .slideToggle(1000);
     }
   });
 });
