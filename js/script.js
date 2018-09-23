20fab1c70391ece291c18f61e18bdc920eea0c12
elnurramazanzade <elnur.ramazanzade@mail.ru>
2018-09-23 18:54:53 +0400

    ISOTOPE

--- js/script.js
+++ js/script.js
@@ -61,39 +61,54 @@ $(document).ready(function() {
   $("#send").on("click", function(e) {
     e.preventDefault();
     var mesDiv = $("#message");
-    mesDiv.children().remove();
-    var message = $(`<div></div>`);
+    $(mesDiv).slideUp("slow");
+    setTimeout(function() {
+      mesDiv.children().remove();
 
-    if ($("#name").val() == "") {
-      var span = $(`<span></span>`).text(`Please enter your name.`);
-      message.append(span);
-      mesDiv.append(message);
-      $(mesDiv).slideToggle("slow");
-    } else if ($("#email").val() == "") {
-      var span = $(`<span></span>`).text(`Please enter valid email address.`);
-      message.append(span);
-      mesDiv.append(message);
-      $(mesDiv).slideToggle("slow");
-    } else if ($("#text").val() == "") {
-      var span = $(`<span></span>`).text(`Please enter your message.`);
-      message.append(span);
-      mesDiv.append(message);
-      $(mesDiv).slideToggle("slow");
-    } else {
-      var name = $("#name").val();
-      var h2 = $(`<h2></h2>`).text(`Email Sent Successfully.`);
-      var p = $(`<p></p>`).text(
-        `Thank you ${name}, your message has been submitted to us.`
-      );
-      message.addClass("success");
-      message.append(h2);
-      message.append(p);
-      console.log(message);
-      mesDiv.append(message);
-      $(mesDiv).slideToggle("slow");
-      $("#contact")
-        .find("form")
-        .slideToggle(1000);
-    }
+      var message = $(`<div></div>`);
+      if ($("#name").val() == "") {
+        var span = $(`<span></span>`).text(`Please enter your name.`);
+        message.append(span);
+        mesDiv.append(message);
+        $(mesDiv).slideToggle("slow");
+      } else if ($("#email").val() == "") {
+        var span = $(`<span></span>`).text(`Please enter valid email address.`);
+        message.append(span);
+        mesDiv.append(message);
+        $(mesDiv).slideToggle("slow");
+      } else if ($("#text").val() == "") {
+        var span = $(`<span></span>`).text(`Please enter your message.`);
+        message.append(span);
+        mesDiv.append(message);
+        $(mesDiv).slideToggle("slow");
+      } else {
+        var name = $("#name").val();
+        var h2 = $(`<h2></h2>`).text(`Email Sent Successfully.`);
+        var p = $(`<p></p>`).text(
+          `Thank you ${name}, your message has been submitted to us.`
+        );
+        message.addClass("success");
+        message.append(h2);
+        message.append(p);
+        mesDiv.append(message);
+        $(mesDiv).slideToggle("slow");
+        $("#contact")
+          .find("form")
+          .slideToggle(1000);
+      }
+    }, 1000);
+  });
+  // WORKS
+  $("#works").isotope({
+    itemSelector: ".work-item",
+    layoutMode: "fitRows"
+  });
+  $("#filter li").on("click", function() {
+    $(this)
+      .siblings()
+      .removeClass("active");
+    $(this).addClass("active");
+    var data = $(this).attr("data-filter");
+    $("#works").isotope({ filter: data });
   });
 });
